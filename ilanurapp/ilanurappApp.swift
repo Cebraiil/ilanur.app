3b08b683f9fb96ce429060f7d4fa89ec6ab51f11
Ayman Jibrael <aymanjibrael@outlook.com>
2023-02-09 07:53:10 +0300

    Initial Commit

---
+++ ilanurapp/ilanurappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ilanurappApp.swift
+//  ilanurapp
+//
+//  Created by Ayman on 9.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ilanurappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
